49bdf4e134fbc5568bcda09d7dc96a2a47786470
werogg <supwer00@gmail.com>
2020-10-13 18:53:30 +0200

    Comments added and minor fixes

--- chat-client.py
+++ chat-client.py
@@ -5,41 +5,44 @@ import sys
 
 class ChatClient:
 
-    def __init__(self, host="localhost", port=5542):
+    def __init__(self, host="localhost", port=5541):
         self.host = host
         self.port = port
         self.client_socket = None
         self.sentence = ""
 
     def open_socket(self):
+        # Open and define a new socket stream via internet
         self.client_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.client_socket.connect((self.host, self.port))
-        self.sentence = bytes(input("Type your nickname:\n"), encoding="utf8")
-        self.client_socket.send(self.sentence)
-        srv_response = self.client_socket.recv(1024)
+        self.client_socket.connect((self.host, self.port))  # Connect the socket to the desired HOST & IP
+        self.sentence = bytes(input("Type your nickname:\n"), encoding="utf8")  # First input will be the client's nick
+        self.client_socket.send(self.sentence)  # Send the sentence via socket
+        srv_response = self.client_socket.recv(1024)  # Receive the server response in a 1024 bytes sentence
         print("Server {}:{} responded: {}".format(self.host, self.port, srv_response))
 
     def communication_handling(self):
         try:
+            # Non stop receiving and printing server responses
             while True:
                 srv_response = self.client_socket.recv(1024)
-                print(srv_response)
+                print(srv_response.decode())
         except socket.error:
             sys.exit(-1)
 
     def run(self):
         self.open_socket()
-        receiving_thread = threading.Thread(target=self.communication_handling)
+        receiving_thread = threading.Thread(target=self.communication_handling)  # Open thread for server communication
         receiving_thread.start()
 
         try:
             while True:
-                self.sentence = bytes(input(""), encoding="utf8")
-                self.client_socket.send(self.sentence)
+                self.sentence = bytes(input(""), encoding="utf8")  # Get user input and encode it to bytes
+                self.client_socket.send(self.sentence)  # Send the sentence via socket
         except socket.error:
             sys.exit(-1)
 
 
 if __name__ == "__main__":
-    chat_client = ChatClient()
+    port = int(input("Port to connect:\n"))
+    chat_client = ChatClient(port=port)
     chat_client.run()
--- chat-server.py
+++ chat-server.py
@@ -5,7 +5,7 @@ import sys
 
 class ChatServer:
 
-    def __init__(self, host='localhost', port=5542):
+    def __init__(self, host='localhost', port=5541):
         self.clients = []
         self.host = host
         self.port = port
@@ -16,33 +16,35 @@ class ChatServer:
     def command_handling(self):
         while True:
 
-            user_input = input('Waiting for commands...\n')
+            user_input = input('Waiting for server communications...\n')
 
             if user_input == "list":
+                # List all connected users with their info
                 for client in self.clients:
                     if client.online:
                         print("Name: {}\nIP: {}".format(client.name, client.address))
-            elif user_input == "exit":
-                self.close()
             else:
                 pass
 
     def client_handling(self, new_client):
         try:
+            # Receive the first 1024 bytes as client's nickname and define it
             sentence = new_client.client_socket.recv(1024)
             new_client.name = sentence
             identification_msg = bytes("Client {} has logged in.".format(new_client.name), encoding="utf8")
             print(identification_msg)
-            new_client.client_socket.send(identification_msg)
+            new_client.client_socket.send(identification_msg)  # Send to the client the login confirmation
 
+            # Send client login confirmation to every connected client with an open socket
             for client in self.clients:
                 if client.online:
                     client.client_socket.send(identification_msg)
 
+            # Non stop receiving sentences from the client socket and resending it to all the clients
             while True:
-                message = new_client.client_socket.recv(10242)
+                message = new_client.client_socket.recv(1024)
                 serv_response = bytes("{} -> {}".format(new_client.name, message), encoding="utf8")
-                print(serv_response)
+                print(serv_response.decode(encoding="utf-8"))
 
                 for client in self.clients:
                     if client.online:
@@ -58,17 +60,21 @@ class ChatServer:
         sys.exit(0)
 
     def run(self):
-        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        self.server_socket.bind((self.host, self.port))
-        self.server_socket.listen(1)
+        self.server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)  # Open a new socket stream
+        self.server_socket.bind((self.host, self.port))  # Bind the new socket to a specific HOST & IP
+        self.server_socket.listen(1)  # Let the server accept new connections
+        # Create and start new thread to handle server commands
         self.command_handling_thread = threading.Thread(target=self.command_handling)
         self.command_handling_thread.start()
 
+        # Keep main thread in a infinite loop listening for new connections
         while True:
             client_socket, address = self.server_socket.accept()
-            print("incoming connection accepted")
+            print("Incoming connection accepted")
+
+            # Create a new object and thread to handle the communication between the server and the new client
             new_client = Client(client_socket, "undefined", True, address)
-            self.clients.append(new_client)
+            self.clients.append(new_client)  # We add the Client's object to a common pool
             self.client_handling_thread = threading.Thread(target=self.client_handling, args=(new_client,))
             self.client_handling_thread.start()
 
@@ -83,5 +89,6 @@ class Client:
 
 
 if __name__ == "__main__":
-    chat_server = ChatServer()
+    port = int(input("Port to connect:\n"))
+    chat_server = ChatServer(port=port)
     chat_server.run()
